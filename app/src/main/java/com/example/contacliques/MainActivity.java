3977cb6b36725030c58708692d06189e5564da11
Laaioner <pedropiovesan2002@gmail.com>
2025-03-19 19:43:02 -0300

    Projeto finalizado

--- app/src/main/java/com/example/contacliques/MainActivity.java
+++ app/src/main/java/com/example/contacliques/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.contacliques;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int num = 0;
+    TextView txtCont;
+    Button btnAdicionar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txtCont = findViewById(R.id.txtCont);
+        btnAdicionar = findViewById(R.id.btnAdicionar);
+
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num++;  // Incrementa o contador
+                txtCont.setText("Quantidade: " + num);
+            }
         });
     }
 }
